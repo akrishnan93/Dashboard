aabfeadb13cdcdac9ae6ef8aef47fcb09ebe682e
akrishnan93 <anand.krrishnan@gmail.com>
2022-01-29 10:29:36 -0500

    added bg for home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import { useNavigation } from '@react-navigation/core'
 import React from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, ImageBackground } from 'react-native'
 import { auth } from '../firebase'
+import DateTime from '../components/DateTime.js'
 
 const HomeScreen = () => {
   const navigation = useNavigation()
+  const image = require('../assets/bg_image.png')
 
   const handleSignOut = () => {
     auth
@@ -17,13 +19,15 @@ const HomeScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text>Email: {auth.currentUser?.email}</Text>
-      <TouchableOpacity
-        onPress={handleSignOut}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Sign out</Text>
-      </TouchableOpacity>
+      <ImageBackground source={image} style = {styles.image}>
+        <Text>Email: {auth.currentUser?.email}</Text>
+        <TouchableOpacity
+          onPress={handleSignOut}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Sign out</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   )
 }
@@ -33,8 +37,6 @@ export default HomeScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
   },
    button: {
     backgroundColor: '#0782F9',
@@ -49,4 +51,5 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     fontSize: 16,
   },
+  image: {flex:1, resizeMode: "cover", justifyContent: "center"}
 })
